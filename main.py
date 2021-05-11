fed9178a44872b62673105247b88180cad2e8a1d
Andromeda606 <43077625+Andromeda606@users.noreply.github.com>
2021-05-11 03:53:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 import settings
+import random
+import string
 
-mails = open("mails.txt", "r", encoding="utf8").read().split("\n")
-PASSWORD = "123PaswwordEv!"
+print("----------------------")
+print("Androsoft Trendyol Hesap Açıcı")
+print("R10 Androsoft")
+print("Instagram: @ramazan_3_")
+print("----------------------")
+print()
+print("Hesap Açma Türünü Seçin")
+print("1- Mail listesi (Max 10 mail, fazlasında trendyol ip nizi kısıtlıyor.)")
+print("2- Random Mailler ile")
 
-for x in mails:
-    settings.new_account(x,PASSWORD)
+def random_str(length):
+    return ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(length))
+
+
+while True:
+    status = input("")
+    if status == "1": #int e çevirebilirdim ama gerek yok.
+        passw = input("Açılacak hesapların şifresini giriniz: ")
+
+        mails = open("openedmails.txt", "r", encoding="utf8").read().split("\n")
+
+        for x in mails:
+            print(settings.new_account(x, passw))
+
+        print("Bizi tercih ettiğiniz için teşekkürler, bütün hesaplar açıldı!")
+
+    elif status == "2":
+
+        passw = input("Açılacak hesapların şifresini giriniz: ")
+
+        for x in range(10):
+            print(settings.new_account(random_str(15) + "@gmail.com", passw))
+
+        print("Bizi tercih ettiğiniz için teşekkürler, bütün hesaplar açıldı!")
+
+    else:
+        print("Lütfen tekrar gir")
+        continue
