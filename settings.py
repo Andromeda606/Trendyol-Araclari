a593fed10b0d6a6b5894cf763236b57e76b6ff38
Ramazan Akbal <43077625+Andromeda606@users.noreply.github.com>
2022-04-16 16:03:10 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ import string
 import os
 import time
 from urllib.parse import urlparse
+from temp_mail import Mail
 
 try:
     import requests
@@ -11,7 +12,6 @@ except:
     os.system("python -m pip3 install requests")
     exit("requests MODÜLÜ YÜKLÜ DEĞİL! \"pip install requests\" veya \"pip3 install requests\" yazarak "
          "indirebilirsiniz. (Otomatik yüklemeyi denedi, önce tekrar çalıştırmayı deneyin)")
-
 
 
 def adb(code):
@@ -33,6 +33,7 @@ def airplane_change():
     change_airplane(False)
     time.sleep(4)
 
+
 def get_rand_token():
     return random_str(8) + "-" + random_str(4) + "-" + "-" + random_str(4) + "-" + random_str(4) + "-" + random_str(13)
 
@@ -41,36 +42,30 @@ def random_str(length):
     return ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(length))
 
 
-def new_account(mail, passw):
-    url = "https://loginapp.trendyol.com/register/user"
+def new_account(mail, passw, otp=None):
+    email = None
+    url = "https://auth.trendyol.com/v2/signup"
+    if mail is None:
+        email = Mail()
+        mail = email.email
 
     payload = {
-        "guestToken": "",
-        "preferences": [
-            {
-                "id": 0,
-                "isAccept": True
-            }
-        ],
-        "regulation": {
-            "isConditionOfMembershipApproved": True,
-            "isProtectionOfPersonalDataApproved": True
-        },
-        "user": {
-            "email": mail,
-            "gender": 2,
-            "password": passw,
-            "storeFrontId": "1",
-            "userType": "MEMBER"
-        }
+        "email": mail,
+        "marketingEmailsAuthorized": False,
+        "password": passw,
+        "genderId": -1,
+        "conditionOfMembershipApproved": True,
+        "protectionOfPersonalDataApproved": True,
+
     }
+    if otp is not None:
+        payload["otpCode"] = otp
 
     headers = {
-        "Host": "loginapp.trendyol.com",
         "X-Storefront-Id": "1",
         "X-Application-Id": "5",
         "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 7.1.1; ONEPLUS A5000 Build/NMF26X) Trendyol/5.10.3.521",
-        'Build': "5.10.3.521",
+        'Build': "6.13.3.616",
         "Platform": "Android",
         "Gender": "F",
         "Searchsegment": "31",
@@ -82,49 +77,30 @@ def new_account(mail, passw):
         "Uniqueid": random_str(16),
         "Content-Type": "application/json; charset=UTF-8",
         "Accept-Encoding": "gzip, deflate",
-        "Connection": "close"
+        "Culture": "tr-TR",
+        "Storefront-Id": "1",
+        "Application-Id": "1"
     }
 
-    response = requests.request("POST", url, json=payload, headers=headers)
+    response = requests.post(url, json=payload, headers=headers)
 
     if response.status_code == 429:
         return "IPCHANGE"
     else:
-        if "accessToken" in response.text:
+        if otp is None:
+            print(mail + " Maili ile hesap açılıyor.")
+            if email is not None:
+                print("Onay kodu aranıyor, biraz uzun sürebilir.")
+                res = new_account(mail, passw, email.get_verification_code())
+            else:
+                res = new_account(mail, passw, input("Onay Kodunu Giriniz"))
+        else:
+            return response.text
+        if "accessToken" in res:
             f = open("openedmails.txt", "a")
             f.write(mail + ":" + passw + "\n")
             f.close()
-        return (response.text)
-
-
-def favorite_item(id,bearer):
-    url = "https://api.trendyol.com/webaccountgw/api/favorites/?storefrontId=1&culture=tr-TR"
-
-    payload = {
-        "itemNumbers": [],
-        "brandId": 682,
-        "brandName": "Slazenger",
-        "categoryId": 429,
-        "categoryName": "Yürüyüş Ayakkabısı",
-        "contentId": id,
-        "contentName": "Erkek Siyah Yürüyüş Ayakkabısı",
-        "favoritedPrice": 118.7
-    }
-
-    headers = {
-        "Authorization": "Bearer " + bearer,
-        "Connection": "keep-alive",
-        "Content-Type": "application/json;charset=UTF-8",
-        "Host": "api.trendyol.com",
-        "Origin": "https://www.trendyol.com",
-        "Referer": "https://www.trendyol.com/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
-    }
-
-    response = requests.request("POST", url, json=payload, headers=headers)
-    print(response.text)
-    print(response.status_code)
-
+        return (res)
 
 def login(email, passw):
     url = "https://loginapp.trendyol.com/auth/token"
@@ -197,13 +173,14 @@ def loginMobile(email, passw):
     }
 
     response = requests.request("POST", url, json=payload, headers=headers)
-    
+
     if response.status_code == 429:
         return "ERROR429"
     return response.json()
 
 
 def get_cupons(bearer):
+
     url = "https://zeusapi.trendyol.com/mobile-zeus-zeuscheckout-service/coupon"
 
     querystring = {"page": "1", "couponContext": "COUPON"}
@@ -233,9 +210,9 @@ def get_cupons(bearer):
     response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
 
     return response.json()
-        
 
-def go_save(id,bearer):
+
+def go_save(id, bearer):
     url = "https://browsingpublic-mdc.trendyol.com/mobile-zeus-zeussocial-service/zeus/mycollections/" + id + "/follow"
 
     headers = {
@@ -265,12 +242,14 @@ def go_save(id,bearer):
     print(response.text)
     print(response.status_code)
 
+
 def query_to_dict(query: str) -> dict:
     returned = {}
     for x in query.split("&"):
         element = x.split("=")
         returned[element[0]] = element[1]
     return returned
+
 
 def get_collection_id(link) -> str:
     # ID yi fark etmemi sağlayan "do6an" adlı Github kullanıcısına teşekkürler.
@@ -288,4 +267,3 @@ def get_collection_id(link) -> str:
     elif "k-" in link:
         path = urlparse(link).path
         return path[path.index("k-") + 2:]
-
